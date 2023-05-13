b4f816a089adf8b6f27f5fc7593880bbf0f63fb8
Kazuya Takei <myself@attakei.net>
2023-05-13 11:15:33 +0900

    chore: Sync from latest cookiecutter

--- docs/conf.py
+++ docs/conf.py
@@ -1,11 +1,11 @@
 # noqa: D100
-from atsphinx.feed import __version__
+from atsphinx.feed import __version__ as version
 
 # -- Project information
-project = "atsphinx-feed"
+project = "atsphinx-patch"
 copyright = "2023, Kazuya Takei"
 author = "Kazuya Takei"
-release = __version__
+release = version
 
 # -- General configuration
 extensions = [
